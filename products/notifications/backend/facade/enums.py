e5a3ca1cbb2a8f041ca85ec73b3b0891de5de0fe
Yasen Slavov <yasen@posthog.com>
2026-05-01 16:00:18 +0300

    feat(notifications): add pipeline_failure to NotificationType

--- products/notifications/backend/facade/enums.py
+++ products/notifications/backend/facade/enums.py
@@ -7,6 +7,7 @@ from posthog.scopes import APIScopeObject
 
 class NotificationType(str, Enum):
     COMMENT_MENTION = "comment_mention"
+    PIPELINE_FAILURE = "pipeline_failure"
 
 
 class Priority(str, Enum):
